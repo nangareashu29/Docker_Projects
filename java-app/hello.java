ce4dbd26f2030a522c9e458c49a86305903eb37d
nangareashu29 <nangareashu94@gmail.com>
2024-02-28 17:05:40 +0000

    java-app added

---
+++ java-app/hello.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class hello{
+	public static void main(String[] args){
+		System.out.println("hello Dosto");
+		Date currentDate = new Date();
+		System.out.println("today's date is: "+ currentDate);
+	}
+}
